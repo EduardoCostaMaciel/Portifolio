267a6a7a4a8babc2fc182303702ce3128db20ccf
ECM <costaeduardomaciel@gmail.com>
2023-11-23 17:08:57 -0300

    Feat: projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,26 +1,77 @@
-import { Header } from '../components/Header';
-
-// interface IPropsProjects {
-//   setTitlePage: React.Dispatch<React.SetStateAction<string>>;
-// }
+import { IconLink } from '../components/icons';
+import { colors } from '../utils/colors';
+import { dataProjects } from '../utils/dataProjects';
 
 export function Projects() {
-  const titlePage = 'Projects';
-  // setTitlePage(titlePage);
   return (
-    <section
-      id='projects'
-      className='
-        w-full
-        h-screen
-        flex flex-col items-center
-        bg-primary-200
-        text-zinc-400
-        transition-all ease-out duration-700
-      '
-    >
-      <Header text={titlePage} />
-      Em breve
-    </section>
+    <>
+      <hr
+        id='projects'
+        className='w-11/12'
+      />
+      <section className='flex min-h-screen w-11/12 flex-col justify-around divide-y divide-gray-600 py-4 text-sm text-gray-300 transition-all duration-500 ease-out'>
+        {dataProjects.featured?.map((item) => (
+          <div
+            key={item.name}
+            className='flex w-full flex-wrap justify-center gap-2 px-6 py-4 md:flex-nowrap md:justify-between md:gap-4'
+          >
+            <div className='order-2 flex flex-col gap-y-4 md:order-1 md:w-3/5'>
+              <h3 className='text-center text-xl text-secondary-2 lg:text-start'>
+                {item.name}
+              </h3>
+              <p className='text-center lg:text-start'>{item?.description}</p>
+              <hr className='' />
+
+              <div className='flex gap-x-2'>
+                {item?.tags?.map((tag, index) => (
+                  <span key={index}>{tag}</span>
+                ))}
+              </div>
+
+              <div className='flex gap-x-4'>
+                <a
+                  href={item?.url}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='flex items-center gap-x-1 font-medium text-secondary-2 hover:text-white'
+                >
+                  <IconLink
+                    size='24px'
+                    fill={colors.secondary[2]}
+                  />
+                  Pagina Web
+                </a>
+                <a
+                  href={item?.repo}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='flex items-center gap-x-1 font-medium text-secondary-2 hover:text-white'
+                >
+                  <IconLink
+                    size='24px'
+                    fill={colors.secondary[2]}
+                  />
+                  Repositório Github
+                </a>
+              </div>
+            </div>
+
+            <img
+              src={item?.imageSrc}
+              alt={item?.name}
+              className='order-1 w-72 rounded-md bg-black shadow-md shadow-gray-900 md:order-2'
+            />
+          </div>
+        ))}
+        <div className='flex flex-col gap-10 p-4'>
+          <span>Em breve mais...</span>
+          <div className='flex items-center justify-center text-2xl sm:justify-start sm:pl-6'>
+            <span className='flex animate-bounce items-center justify-center rounded bg-white bg-opacity-30 px-6 py-1 text-primary-1 backdrop-blur-sm'>
+              Projetos
+            </span>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
---
+++ src/utils/dataProjects.ts
@@ -0,0 +1,18 @@
+import imgAppRecipes from '../assets/images/appRecipes_2.png';
+
+export const dataProjects = {
+  id: 'projects',
+  name: 'Projects',
+  path: '/projects',
+  featured: [
+    {
+      name: 'App Recipes',
+      url: 'https://app-recipes-ecm.vercel.app/',
+      repo: 'https://github.com/EduardoCostaMaciel/App_Recipes',
+      imageSrc: imgAppRecipes,
+      description:
+        'Explore uma vasta coleção de deliciosas receitas de pratos e bebidas de todo o mundo. Encontre inspiração para sua próxima aventura culinária, com detalhes completos das receitas, incluindo a lista de ingredientes e vídeos passo a passo para orientá-lo. Além disso, você pode marcar suas receitas favoritas e criar sua própria coleção pessoal.',
+      tags: ['React.js(CRA)']
+    }
+  ]
+};
